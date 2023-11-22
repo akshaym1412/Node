85d4259ec6e6db57c6e8d314e2e185d1c22c7333
Akshay_M <harshaakshay14@gmail.com>
2023-11-22 13:51:10 +0530

    files added

--- database/db.js
+++ database/db.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const connection=(async(URL)=>{
-    const URL={URL};
     try{
         await mongoose.connect(URL, { useNewUrlParser: true })
         console.log("Database Connected");
